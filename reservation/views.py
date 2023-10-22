534f2f87c17a622175f23a11d96a766df2ecc307
PatKredatus <pat.kredatus@gmail.com>
2023-10-22 10:45:49 +0200

    vsdfv

--- reservation/views.py
+++ reservation/views.py
@@ -1,20 +1,16 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Reservation
 from datetime import datetime, timedelta
 from django.contrib import messages
 
 
-@login_required
 def checkout(request):
-    user = request.user
+    if request.user:
+        user = request.user
     dates = request.GET.get('dates')
 
     if not dates:
         messages.error(request, 'Vyberte si termín.')
-        return redirect('reservations')
-
-    if not user:
         return redirect('reservations')
 
     dates = dates.split(',')
@@ -27,22 +23,26 @@ def checkout(request):
     else:
         start_date = datetime.strptime(dates[0], '%Y-%m-%d')
         end_date = datetime.strptime(dates[1], '%Y-%m-%d')
+        if end_date < start_date:
+            start_date = datetime.strptime(dates[1], '%Y-%m-%d')
+            end_date = datetime.strptime(dates[0], '%Y-%m-%d')
 
     while start_date <= end_date:
         selected_dates_by_user.append(start_date.strftime('%Y-%m-%d'))
         start_date += timedelta(days=1)
 
     if len(selected_dates_by_user) <3:
-        messages.error(request, 'Minimálne 2 noci.')
+        messages.error(request, 'Vyberte si minimálne 2 noci.')
         return redirect('reservations')
 
     # Retrieve all reservations from the database
     reservations = Reservation.objects.all()
 
     # Iterate through reservations to extract reserved dates
+    # last day of reservation can be reserved again
     for reservation in reservations:
         start_date = reservation.date_from
-        end_date = reservation.date_to - timedelta(days=1) # last day of reservation can be reserved again
+        end_date = reservation.date_to - timedelta(days=1)
 
         # Generate a list of dates within the reservation range
         while start_date <= end_date:
@@ -54,4 +54,4 @@ def checkout(request):
             messages.error(request, 'Váš termín je už obsadený.')
             return redirect('reservations')
 
-    return render(request, 'checkout.html')
+    return render(request, 'checkout.html', {'dates': selected_dates_by_user})
